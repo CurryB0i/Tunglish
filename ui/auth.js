4afbbfdc1f9b734cb964d16909e876d054556671
CurryBoi <akarshrai05@gmail.com>
2026-04-02 19:25:45 +0530

    fix(bus): many bugs man

--- ui/auth.js
+++ ui/auth.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "https://tunglish.onrender.com";
+export const API_BASE_URL = "http://localhost:8000";
 const form = document.getElementsByTagName('form')[0];
 
 const user = {
