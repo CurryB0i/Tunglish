382726aa31ac5ea8300a5b844bb55bd61747a38f
CurryBoi <akarshrai05@gmail.com>
2026-04-02 19:38:11 +0530

    add(tunglish): render url

--- ui/auth.js
+++ ui/auth.js
@@ -1,4 +1,4 @@
-export const API_BASE_URL = "http://localhost:8000";
+export const API_BASE_URL = "https://tunglish.onrender.com";
 const form = document.getElementsByTagName('form')[0];
 
 const user = {
